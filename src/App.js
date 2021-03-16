3a11c0579acafbe51b9523ee0f30b576a5163c78
Umutcan GÖZEGÜ <38477751+UGzeg@users.noreply.github.com>
2021-03-17 00:30:45 +0300

    creating draft layout & adding axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="slider">
+      <div className="sliderHeader">
+        <div className="sliderTitle">
+          Popüler Olanlar
+        </div>
+        <div className="sliderTab">
+          <a href="#">Tab 1</a>
+          <a href="#">Tab 2</a>
+          <a href="#">Tab 3</a>
+        </div>
+      </div>
+      <div className="sliderBody">
+        <div className="sliderItem"></div>
+        <div className="sliderItem"></div>
+        <div className="sliderItem"></div>
+        <div className="sliderItem"></div>
+        <div className="sliderItem"></div>
+      </div>
+      </div>
     </div>
   );
 }
